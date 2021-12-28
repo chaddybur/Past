f43b1249953d5c3bb58d5de61331da0384fd3d5f
Chad Cornwell <chadcornwell86@yahoo.com>
2021-12-28 10:10:10 -0600

    first try

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
-Solution
-1
-function leo(oscar){
-2
-  
-3
-  if(oscar == 86){
-4
-    return "Not even for Wolf of wallstreet?!"
-5
-  }
-6
-​
-7
-  else if (oscar < 88){
-8
-    return "When will you give Leo an Oscar?"
-9
-  }else if(oscar == 88){
-10
-    return "Leo finally won the oscar! Leo is happy"
-11
-  }else {
-12
-    return "Leo got one already!"
-13
-  }
-14
-}
+var quote = function(fighter) {
+    // your code here
+    let name = fighter.toLowerCase()
+    if (name == 'george saint pierre'){
+      return "I am not impressed by your performance."
+    }else{
+      return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    }
+  };
