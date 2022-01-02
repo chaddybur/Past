bfa55e8f88553d13799c253971316bcf8bc3326f
Chad Cornwell <chadcornwell86@yahoo.com>
2022-01-02 10:15:10 -0600

    pushing that stuff

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const solve = (x, y) => x / y
+function uefaEuro2016(teams, scores){
+
+  
+    if(scores[0] > scores[1]){
+      return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
+    }else if(scores[0] < scores[1]){
+      return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+    }else{
+     return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
+    }
+      
+  }
