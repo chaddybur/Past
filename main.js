dd2778459945f1d219f1bdcb2d6f1f7f27456433
Chad Cornwell <chadcornwell86@yahoo.com>
2022-01-03 10:11:12 -0600

    first try

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-function uefaEuro2016(teams, scores){
-
-  
-    if(scores[0] > scores[1]){
-      return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
-    }else if(scores[0] < scores[1]){
-      return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
-    }else{
-     return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
-    }
-      
+function pointsPer48(ppg, mpg) {
+  if (ppg == 0 || ppg ==0){
+    return 0
+  }
+    let num = (48 / mpg) * ppg
+    let number = num.toFixed(1)
+    let ans = Number(number)
+    
+    return ans
   }
