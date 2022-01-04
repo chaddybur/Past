7829798714968babcc338daa3b8d5eff5d369edf
Chad Cornwell <chadcornwell86@yahoo.com>
2022-01-04 11:00:00 -0600

    first try

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-function pointsPer48(ppg, mpg) {
-  if (ppg == 0 || ppg ==0){
-    return 0
-  }
-    let num = (48 / mpg) * ppg
-    let number = num.toFixed(1)
-    let ans = Number(number)
-    
-    return ans
-  }
+function all( arr, fun ){
+  // ...
+  return arr.every(e => fun(e))
+}
