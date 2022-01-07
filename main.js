d54099f9430a0ba681feb7fd4b3c67584dc36378
Chad Cornwell <chadcornwell86@yahoo.com>
2022-01-07 10:10:10 -0600

    first try

--- main.js
+++ main.js
@@ -1,38 +1,12 @@
-function fuelPrice(litres, pricePerLitre) {
-  2
-    // your code here
-  3
-    // Good luck with it!
-  4
-    console.log(litres, pricePerLitre)
-  5
-    
-  6
-    if(litres < 2){
-  7
-      return Number((litres * pricePerLitre).toFixed(2))
-  8
-    }else if(litres < 4){
-  9
-      return Number((litres * (pricePerLitre - .05)).toFixed(2))
-  10
-    }else if(litres < 6){
-  11
-      return Number((litres * (pricePerLitre - .1)).toFixed(2))
-  12
-    }else if(litres < 8){
-  13
-      return Number((litres * (pricePerLitre - .15)).toFixed(2))
-  14
-    }else if(litres < 10){
-  15
-      return Number((litres * (pricePerLitre - .2)).toFixed(2))
-  16
+function padIt(str,n){
+  let i = 1
+  while(i <= n){
+    if(i % 2 == 0){
+      str = str + "*"
+      i++
     }else{
-  17
-      return Number((litres * (pricePerLitre - .25)).toFixed(2))
-  18
+       str = "*"+ str
+      i++
     }
-  19
-  }
-  
+  }return str
+}
