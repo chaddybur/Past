67aeec455423be84b2b1cbb4e369aa85d9500b5c
Chad Cornwell <chadcornwell86@yahoo.com>
2022-01-13 09:10:45 -0600

    first try

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function charFreq(message) {
-  // awesome counter code
-    let data = message.split("")
-    const obj = data.reduce(function(obj, item) {
-        if (!obj[item]) {
-          obj[item] = 0;
-        }
-        obj[item]++;
-        return obj;
-      }, {});
+function oddOrEven(array) {
+  console.log(array)
+ 
+  let num = array.reduce((a,b) => a+b, 0)
+  
+  if(num % 2 == 0){
+    return "even"
+  }else{
+    return "odd"
+  }
 }
