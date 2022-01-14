483a2d942836a08d27d9a8566e43c029e0ada9e3
Chad Cornwell <chadcornwell86@yahoo.com>
2022-01-14 09:09:04 -0600

    first try

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-function oddOrEven(array) {
-  console.log(array)
- 
-  let num = array.reduce((a,b) => a+b, 0)
+Solution
+1
+function narcissistic(value) {
+2
+  let number = 0
+3
+  let arr = value.toString().split("")
+4
   
-  if(num % 2 == 0){
-    return "even"
-  }else{
-    return "odd"
+5
+  for(i = 0; i < arr.length; i++){
+6
+    
+7
+    number += Number(arr[i]) ** arr.length
+8
   }
+9
+  if(number == value){
+10
+    return true
+11
+  }else{
+12
+    return false
+13
+  }
+14
 }
+15
+​
+ Correct! You may take your time to refactor/comment your solution. Submit when ready.
