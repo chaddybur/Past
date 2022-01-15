81bb4b72ac0550302541ef06648bd99b80b2f112
Chad Cornwell <chadcornwell86@yahoo.com>
2022-01-15 10:10:10 -0600

    first try

--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-Solution
-1
-function narcissistic(value) {
-2
-  let number = 0
-3
-  let arr = value.toString().split("")
-4
-  
-5
-  for(i = 0; i < arr.length; i++){
-6
-    
-7
-    number += Number(arr[i]) ** arr.length
-8
-  }
-9
-  if(number == value){
-10
-    return true
-11
-  }else{
-12
-    return false
-13
-  }
-14
+function add(a, b){
+	return a == b ? true: false
 }
-15
-​
- Correct! You may take your time to refactor/comment your solution. Submit when ready.
